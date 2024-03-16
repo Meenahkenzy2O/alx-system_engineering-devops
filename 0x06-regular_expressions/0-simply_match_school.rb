1e75bc7cc159a01a950ffa2167df77ed121afd75
meenahkenz <itzamina65@gmail.com>
2024-03-16 04:24:23 +0100

    0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,5 +1,4 @@
+
 #!/usr/bin/env ruby
-## A regular expression that is simply matching School
-#puts ARGV[0].scan(/School/).join#!/usr/bin/env ruby
-## A regular expression that is simply matching School
-#puts ARGV[0].scan(/School/).join
+# A regular expression that is simply matching School
+puts ARGV[0].scan(/School/).join
